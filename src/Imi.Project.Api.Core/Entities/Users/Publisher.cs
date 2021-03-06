08105e39862a9574b7df073f38b71da3332b440c
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-03-06 17:35:23 +0100

    Add Publisher

---
+++ src/Imi.Project.Api.Core/Entities/Users/Publisher.cs
@@ -0,0 +1,12 @@
+﻿using Imi.Project.Api.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Entities.Users
+{
+    public class Publisher : User
+    {
+        public ICollection<BoardGame> PublishedBoardGames { get; set; }
+    }
+}
