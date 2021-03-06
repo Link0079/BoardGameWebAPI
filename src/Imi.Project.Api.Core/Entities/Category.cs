150bc4c4915a41a0e6953e012da91123691fbe94
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-03-06 17:27:27 +0100

    Add Category

---
+++ src/Imi.Project.Api.Core/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using Imi.Project.Api.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Entities
+{
+    public class Category : BaseEntity<Guid>
+    {
+        public string Name { get; set; }
+        public ICollection<BoardGameCategory> BoardGames { get; set; }
+    }
+}
