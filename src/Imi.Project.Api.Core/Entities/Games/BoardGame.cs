7112b2fdf1d3c0829aecaf1b10ecc999c6369b6d
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-03-06 17:23:54 +0100

    Add BoardGame

---
+++ src/Imi.Project.Api.Core/Entities/Games/BoardGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Imi.Project.Api.Core.Entities.Base;
+
+namespace Imi.Project.Api.Core.Entities.Games
+{
+    public class BoardGame : Game
+    {
+        public int Age { get; set; }
+        public int Rating { get; set; }
+        public int PlayTime { get; set; }
+        public int MinPlayers { get; set; }
+        public int MaxPlayers { get; set; }
+        public string Description { get; set; }
+        public bool Stock { get; set; }
+        public Uri PhotoUrl { get; set; }
+        public Guid PublisherId { get; set; }
+        public Publisher Publisher { get; set; }
+        public ICollection<BoardGameCategory> Categories { get; set; }
+        public ICollection<BoardGameArtist> Artists { get; set; }
+
+    }
+}
