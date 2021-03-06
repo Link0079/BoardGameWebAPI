56020ea7ccb784e8d4490ef8ef87e0123420225a
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-03-06 17:29:51 +0100

    Add auxiliary class BoardGameCategory

---
+++ src/Imi.Project.Api.Core/Entities/Games/BoardGameCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Entities.Games
+{
+    public class BoardGameCategory
+    {
+        public Guid BoardGameId { get; set; }
+        public Guid CategoryId { get; set; }
+        public BoardGame BoardGame { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
