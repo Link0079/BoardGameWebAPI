b6548a866ad5330f9c01459908b0bf732ed9ed40
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-03-06 17:31:10 +0100

    Add Artist

---
+++ src/Imi.Project.Api.Core/Entities/Users/Artist.cs
@@ -0,0 +1,13 @@
+﻿using Imi.Project.Api.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Entities.Users
+{
+    public class Artist : User
+    {
+        public int Year { get; set; }
+        public ICollection<BoardGameArtist> Artwork { get; set; }
+    }
+}
