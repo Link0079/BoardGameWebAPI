e25768d547dc097671fc20b6d266596f4a4ae7ff
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-03-06 17:21:09 +0100

    Add Game

---
+++ src/Imi.Project.Api.Core/Entities/Base/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Entities.Base
+{
+    public abstract class Game : BaseEntity<Guid>
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public int Year { get; set; }
+        public Guid CountryId { get; set; }
+        public Country Country { get; set; }
+    }
+}
