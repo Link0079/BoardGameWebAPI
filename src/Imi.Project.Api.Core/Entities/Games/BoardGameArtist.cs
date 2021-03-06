059a0f00af76a1ae6d14468c3122654eff5e4696
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-03-06 17:36:48 +0100

    Add ausiliary class BoardGameArtist

---
+++ src/Imi.Project.Api.Core/Entities/Games/BoardGameArtist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Entities.Games
+{
+    public class BoardGameArtist
+    {
+        public Guid BoardGameId { get; set; }
+        public Guid ArtistId { get; set; }
+        public BoardGame BoardGame { get; set; }
+        public Artist Artist { get; set; }
+    }
+}
