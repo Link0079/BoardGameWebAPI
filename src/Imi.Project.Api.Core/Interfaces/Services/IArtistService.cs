eef62cc38172aa034f245481ae79fb83b6d96f8d
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-03-14 21:00:39 +0100

    Add IArtistService

---
+++ src/Imi.Project.Api.Core/Interfaces/Services/IArtistService.cs
@@ -0,0 +1,13 @@
+﻿using Imi.Project.Api.Core.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Core.Interfaces.Services
+{
+    public interface IArtistService
+    {
+        Task<IEnumerable<ArtistResponseDto>> ListAllAsync();
+        Task<ArtistResponseDto> GetByIdAsync();
+
+    }
+}
