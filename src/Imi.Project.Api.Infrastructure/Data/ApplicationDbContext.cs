4e445182f4e090609c164e943170887df47aac0a
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-03-14 17:48:14 +0100

    Add tables to ApplicationDbContext

--- src/Imi.Project.Api.Infrastructure/Data/ApplicationDbContext.cs
+++ src/Imi.Project.Api.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using Imi.Project.Api.Core.Entities;
+using Imi.Project.Api.Core.Entities.Games;
+using Imi.Project.Api.Core.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Imi.Project.Api.Infrastructure.Data
 {
-    class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<BoardGame> BoardGames { get; set; }
+        public DbSet<BoardGameArtist> BoardGameArtists { get; set; }
+        public DbSet<BoardGameCategory> BoardGameCategories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        { 
+        
+        }
+
     }
 }
