68c4fe70d7de0ad0615ed411156192f2205ce40c
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-03-14 18:01:08 +0100

    Add modelbuilding in ApplicationDbContext

--- src/Imi.Project.Api.Infrastructure/Data/ApplicationDbContext.cs
+++ src/Imi.Project.Api.Infrastructure/Data/ApplicationDbContext.cs
@@ -21,8 +21,67 @@ namespace Imi.Project.Api.Infrastructure.Data
         public DbSet<BoardGameCategory> BoardGameCategories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        { 
-        
+        {
+            #region Categories Table
+            modelBuilder.Entity<Category>().ToTable("Categories").HasKey(e => e.Id);
+            modelBuilder.Entity<Category>().Property(e => e.Name)
+                .HasMaxLength(100).IsRequired();
+            #endregion
+
+            #region Countries Table
+            modelBuilder.Entity<Country>().ToTable("Countries").HasKey(e => e.Id);
+            modelBuilder.Entity<Country>().Property(e => e.Name)
+                .HasMaxLength(100).IsRequired();
+            modelBuilder.Entity<Country>().Property(e => e.CountryCode)
+                .HasMaxLength(3);
+            #endregion
+
+            #region Publishers Table
+            modelBuilder.Entity<Publisher>().ToTable("Publishers").HasKey(e => e.Id);
+            modelBuilder.Entity<Publisher>().Property(e => e.Name)
+                .HasMaxLength(100).IsRequired();
+            #endregion
+
+            #region Artists Table
+            modelBuilder.Entity<Artist>().ToTable("Artists").HasKey(e => e.Id);
+            modelBuilder.Entity<Artist>().Property(e => e.Name)
+                .HasMaxLength(100).IsRequired();
+            #endregion
+
+            #region Boadgames Table
+            modelBuilder.Entity<BoardGame>().ToTable("BoardGames").HasKey(e => e.Id);
+            modelBuilder.Entity<BoardGame>().Property(e => e.Title)
+                .HasMaxLength(255).IsRequired();
+            modelBuilder.Entity<BoardGame>().Property(e => e.Price)
+                .HasColumnType("money");
+            //
+            //
+            #endregion
+
+            #region BoardGameArtist Tabel
+            modelBuilder.Entity<BoardGameArtist>().ToTable("BoardGameArtists")
+                .HasKey(ba => new { ba.BoardGameId, ba.ArtistId });
+            modelBuilder.Entity<BoardGameArtist>().HasOne(ba => ba.BoardGame)
+                .WithMany(a => a.Artists).HasForeignKey(ba => ba.BoardGameId)
+                .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<BoardGameArtist>().HasOne(ba => ba.Artist)
+                .WithMany(b => b.Artwork).HasForeignKey(ba => ba.ArtistId)
+                .OnDelete(DeleteBehavior.Restrict);
+            #endregion
+
+            #region BoardGameCategory Table
+            modelBuilder.Entity<BoardGameCategory>().ToTable("BoardGameCategories")
+                .HasKey(bc => new { bc.BoardGameId, bc.CategoryId });
+            modelBuilder.Entity<BoardGameCategory>().HasOne(bc => bc.BoardGame)
+                .WithMany(c => c.Categories).HasForeignKey(bc => bc.BoardGameId)
+                .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<BoardGameCategory>().HasOne(bc => bc.Category)
+                .WithMany(b => b.BoardGames).HasForeignKey(bc => bc.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+            #endregion
+
+
+            base.OnModelCreating(modelBuilder);
         }
 
     }
