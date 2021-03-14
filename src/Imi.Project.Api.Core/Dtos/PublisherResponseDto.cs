18087152a0a21c833a5c733e3e6d8b829e7f66bb
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-03-14 20:38:57 +0100

    Add PublisherResponseDto

---
+++ src/Imi.Project.Api.Core/Dtos/PublisherResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using Imi.Project.Api.Core.Dtos.Base;
+
+namespace Imi.Project.Api.Core.Dtos
+{
+    public class PublisherResponseDto : BaseDto
+    {
+        public string Name { get; set; }
+        public string CountryName { get; set; }
+        public int NumberOfPublishedBoardGames { get; set; }
+    }
+}
