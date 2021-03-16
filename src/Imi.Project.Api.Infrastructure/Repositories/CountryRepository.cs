dd07d02f94635bb3f8fa7b08206165ad632037f5
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-03-17 00:09:16 +0100

    Add CountryRepository to infrastructure

---
+++ src/Imi.Project.Api.Infrastructure/Repositories/CountryRepository.cs
@@ -0,0 +1,26 @@
+﻿using Imi.Project.Api.Core.Entities;
+using Imi.Project.Api.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Infrastructure.Repositories
+{
+    public class CountryRepository : EfRepository<Country>
+    {
+        public CountryRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+        public override IQueryable<Country> GetAllAsync()
+        {
+            return _dbContext.Countries.AsNoTracking();
+        }
+        public override async Task<Country> GetByIdAsync(Guid id)
+        {
+            return await GetAllAsync().SingleOrDefaultAsync(c => c.Id.Equals(id));
+        }
+    }
+}
