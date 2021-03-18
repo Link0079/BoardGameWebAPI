88ada1bbb6fc411685420ce53b6383c3d88d21fd
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-03-18 22:59:11 +0100

    Add Player

---
+++ src/Imi.Project.Api.Core/Entities/Users/Player.cs
@@ -0,0 +1,11 @@
+﻿using Imi.Project.Api.Core.Entities.Base;
+using Imi.Project.Api.Core.Entities.Games;
+using System.Collections.Generic;
+
+namespace Imi.Project.Api.Core.Entities.Users
+{
+    public class Player : User
+    {
+        public ICollection<GameScore> GameScores { get; set; }
+    }
+}
