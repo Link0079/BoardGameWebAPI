4f770ba817f356561822573221b09656732d7175
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-03-18 23:01:19 +0100

    Add GameScore

---
+++ src/Imi.Project.Api.Core/Entities/Games/GameScore.cs
@@ -0,0 +1,18 @@
+﻿using Imi.Project.Api.Core.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Entities.Games
+{
+    public class GameScore
+    {
+        public Guid PlayerId { get; set; }
+        public Guid PlayedGameId { get; set; }
+        public int Score { get; set; }
+        public bool IsDeleted { get; set; }
+        // NaviProperties
+        public Player Player { get; set; }
+        public PlayedGame PlayedGame { get; set; }
+    }
+}
