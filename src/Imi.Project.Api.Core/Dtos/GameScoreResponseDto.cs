12d5af0513a98f5a8c29a7cd9150db5a075a25c9
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-03-20 00:36:20 +0100

    Add PlayedGameService and PlayedGameRepository

---
+++ src/Imi.Project.Api.Core/Dtos/GameScoreResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Dtos
+{
+    public class GameScoreResponseDto
+    {
+        public string PlayerName { get; set; }
+        public int Score { get; set; }
+    }
+}
---
+++ src/Imi.Project.Api.Core/Services/PlayedGameService.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Imi.Project.Api.Core.Dtos;
+using Imi.Project.Api.Core.Interfaces.Repositories;
+using Imi.Project.Api.Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Core.Services
+{
+    public class PlayedGameService : IPlayedGameService
+    {
+        private readonly IPlayedGameRepository _playedGameRepository;
+        private readonly IMapper _mapper;
+        public PlayedGameService(IPlayedGameRepository playedGameRepository, IMapper mapper)
+        {
+            _playedGameRepository = playedGameRepository;
+            _mapper = mapper;
+        }
+        public async Task<PlayedGameResponseDto> GetByIdAsync(Guid id)
+        {
+            var result = await _playedGameRepository.GetByIdAsync(id);
+            var dto = _mapper.Map<PlayedGameResponseDto>(result);
+            return dto;
+        }
+        public async Task<IEnumerable<PlayedGameResponseDto>> ListAllAsync()
+        {
+            var result = await _playedGameRepository.ListAllAsync();
+            var dto = _mapper.Map<IEnumerable<PlayedGameResponseDto>>(result);
+            return dto;
+        }
+    }
+}
