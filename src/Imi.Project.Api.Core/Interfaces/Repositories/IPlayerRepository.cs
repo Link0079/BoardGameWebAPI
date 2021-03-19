0f692ca50b545fcb29d9be82f63d22857ee2ed31
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-03-20 00:01:10 +0100

    Add PlayerService and PlayerRepository

---
+++ src/Imi.Project.Api.Core/Interfaces/Repositories/IPlayerRepository.cs
@@ -0,0 +1,12 @@
+﻿using Imi.Project.Api.Core.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Interfaces.Repositories
+{
+    public interface IPlayerRepository : IRepository<Player>
+    {
+
+    }
+}
