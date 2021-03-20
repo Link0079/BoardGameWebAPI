736c5872ab96cca37045467bd038d4bfc5179aa6
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-03-20 12:35:20 +0100

    Add part of PlayerResponsDto mapping

--- src/Imi.Project.Api.Core/Mapping/AutoMapperProfiles.cs
+++ src/Imi.Project.Api.Core/Mapping/AutoMapperProfiles.cs
@@ -1,37 +1,55 @@
 ﻿using AutoMapper;
+using Imi.Project.Api.Core.Dtos.Users;
+using Imi.Project.Api.Core.Entities.Users;
+using Imi.Project.Api.Core.Extentions;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Imi.Project.Api.Core.Mapping
 {
     public class AutoMapperProfiles : Profile
     {
-        #region Player
+        public AutoMapperProfiles()
+        {
+            #region Player
+            CreateMap<Player, PlayerResponseDto>()
+                .ForMember(dest => dest.PlayedGameCount,
+                    opt => opt.MapFrom(src => src.GameScores
+                    .Where(gs => gs.PlayerId == src.Id).Count()))
+                .ForMember(dest => dest.PlayTimeTotal,
+                    opt => opt.MapFrom(src => src.GameScores
+                    .Where(gs => gs.PlayerId == src.Id)
+                    .Sum(gs => gs.PlayedGame.PlayTime)
+                    .ConvertToStringDuration()));
+                //.ForMember(dest => dest.FavoPlayedGame,
+                //    opt => opt.MapFrom(src => src.GameScores
+                //      .Where(gs => gs.PlayerId == src.Id)
+                //      .GroupBy(gs => gs.PlayedGame.BoardGameId)));
+            #endregion
+            #region Category
 
-        #endregion
-        #region Category
+            #endregion
+            #region Artist
 
-        #endregion
-        #region Artist
+            #endregion
+            #region BoardGame
 
-        #endregion
-        #region BoardGame
+            #endregion
+            #region PlayedGame
 
-        #endregion
-        #region PlayedGame
+            #endregion
+            #region GameScore
 
-        #endregion
-        #region GameScore
+            #endregion
 
-        #endregion
+            #region Country
 
-        #region Country
+            #endregion
+            #region Publisher
 
-        #endregion
-        #region Publisher
-
-        #endregion
-
+            #endregion
+        }
     }
 }
