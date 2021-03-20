ea2cce5ff33fcea316b5fedc6408a0e8adb0980f
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-03-20 14:55:35 +0100

    Add Category Get endpoint

---
+++ src/Imi.Project.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,36 @@
+﻿using Imi.Project.Api.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var categories = await _categoryService.ListAllAsync();
+            return Ok(categories);
+        }
+        [HttpGet("{guid}")]
+        public async Task<IActionResult> Get(Guid guid)
+        {
+            var category = await _categoryService.GetByIdAsync(guid);
+            if (category == null)
+                return NotFound($"Category with id {guid} does not exist.");
+            return Ok(category);
+        }
+
+    }
+}
