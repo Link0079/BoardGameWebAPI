abd79337200c84f44f6cafc50b89c9de54f2fe73
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-03-20 15:11:11 +0100

    Add Player Get entpoint

--- src/Imi.Project.Api/Controllers/PlayersController.cs
+++ src/Imi.Project.Api/Controllers/PlayersController.cs
@@ -10,27 +10,26 @@ namespace Imi.Project.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ArtistsController : ControllerBase
+    public class PlayersController : ControllerBase
     {
-        private readonly IArtistService _artistService;
-        public ArtistsController(IArtistService artistService)
+        private readonly IPlayerService _playerService;
+        public PlayersController(IPlayerService playerService)
         {
-            _artistService = artistService;
+            _playerService = playerService;
         }
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            var artists = await _artistService.ListAllAsync();
-            return Ok(artists);
+            var players = await _playerService.ListAllAsync();
+            return Ok(players);
         }
         [HttpGet("{guid}")]
         public async Task<IActionResult> Get(Guid guid)
         {
-            var artist = await _artistService.GetByIdAsync(guid);
-            if (artist == null)
-                return NotFound($"Artist with id {guid} does not exist.");
-            return Ok(artist);
+            var player = await _playerService.GetByIdAsync(guid);
+            if (player == null)
+                return NotFound($"Players with id {guid} does not exist.");
+            return Ok(player);
         }
-
     }
 }
