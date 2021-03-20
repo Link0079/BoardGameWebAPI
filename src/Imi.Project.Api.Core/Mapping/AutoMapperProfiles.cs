448d276c5dae3e7bc1dd749f63e5af1233198668
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-03-20 12:08:50 +0100

    Prepare AutoMapperProfiles

---
+++ src/Imi.Project.Api.Core/Mapping/AutoMapperProfiles.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Mapping
+{
+    public class AutoMapperProfiles : Profile
+    {
+        #region Player
+
+        #endregion
+        #region Category
+
+        #endregion
+        #region Artist
+
+        #endregion
+        #region BoardGame
+
+        #endregion
+        #region PlayedGame
+
+        #endregion
+        #region GameScore
+
+        #endregion
+
+        #region Country
+
+        #endregion
+        #region Publisher
+
+        #endregion
+
+    }
+}
