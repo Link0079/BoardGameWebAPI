78ead895ee5f1e447e5fffb3f49936adde79d52c
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-03-28 13:41:47 +0200

    Refactor Core.Interfaces.Repositories

--- src/Imi.Project.Api.Core/Interfaces/Repositories/IArtistRepository.cs
+++ src/Imi.Project.Api.Core/Interfaces/Repositories/IArtistRepository.cs
@@ -3,11 +3,12 @@ using Imi.Project.Api.Core.Interfaces.Repositories.Base;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Imi.Project.Api.Core.Interfaces.Repositories
 {
     public interface IArtistRepository : IRepository<Artist>
     {
-        // Add more Methodes if needed.
+        Task<IEnumerable<Artist>> SearchByNameAsync(string name);
     }
 }
--- src/Imi.Project.Api.Core/Interfaces/Repositories/ICategoryRepository.cs
+++ src/Imi.Project.Api.Core/Interfaces/Repositories/ICategoryRepository.cs
@@ -3,11 +3,12 @@ using Imi.Project.Api.Core.Interfaces.Repositories.Base;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Imi.Project.Api.Core.Interfaces.Repositories
 {
     public interface ICategoryRepository : IRepository<Category>
     {
-
+        Task<IEnumerable<Category>> SearchByNameAsync(string name);
     }
 }
