a31f03cea7401c84ab7113529ae303eceea0f4bf
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-04-02 21:51:34 +0200

    Add StatisticsResponseDto

---
+++ src/Imi.Project.Api.Core/Dtos/Statistics/StatisticsResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using Imi.Project.Api.Core.Dtos.Games;
+using Imi.Project.Api.Core.Dtos.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Dtos.Statistics
+{
+    public class StatisticsResponseDto
+    {
+        public IEnumerable<BoardGameResponseDto> TopBoardgamePlayTime { get; set; }
+        public IEnumerable<BoardGameResponseDto> TopHighesRatedBoardGames{ get; set; }
+        public IEnumerable<PlayerResponseDto> TopPlayersWithLongestPlayTime { get; set; }
+        public IEnumerable<ArtistResponseDto> TopArtistsWithMostArtwork{ get; set; }
+        public IEnumerable<PlayerResponseDto> TopPlayersWithMostPlayedGames{ get; set; }
+        public IEnumerable<PlayerResponseDto> TopFirstPlayers{ get; set; }
+        public string TotalBoardGamesPlayTime{ get; set; }
+    }
+}
