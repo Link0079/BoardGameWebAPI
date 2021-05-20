2f9f4f8e0f34c725e49c27d02eec47d94c4337a4
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-05-20 22:05:46 +0200

    Add basic boardgame layout to index

--- src/Imi.Project.Vue/wwwroot/js/site.js
+++ src/Imi.Project.Vue/wwwroot/js/site.js
@@ -1,4 +1,36 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿var boardGameApiURL = 'https://localhost:5001/api/boardgames';
+var boardGameApp = new Vue({
+    el: '#boardGameApp',
+    data: {
+        boardgames: null,
+        currentBoardGame: null,
+    },
+    created:
+        function () {
+            let self = this;
+            self.fetchBoardGames();
+        },
+    methods: {
+        fetchBoardGames:
+            function () {
+                let self = this;
+                axios
+                    .get(`${boardGameApiURL}`)
+                    .then(function (response) { self.boardgames = response.data; })
+                    .catch(function (error) {
+                        console.error(error);
+                    });
+            },
+        getBoardGameCssClass:
+            function (boardGame) {
+                if (boardGame.stock == "True") {
+                   return "list-group-item-success";
+                }
+            },
+        getBoardGameDetails:
+            function (boardGame) {
+                let self = this;
+                self.currentBoardGame = boardGame;
+            }
+    }
+});
